e560277f47cde7c400ba00449608ab1526fb680b
SaraGolbek <145944090+SaraGolbek@users.noreply.github.com>
2024-02-08 11:02:19 -0800

    Update index.jsx

--- app/javascript/src/user/index.jsx
+++ app/javascript/src/user/index.jsx
@@ -1,7 +1,7 @@
 // user index.jsx
 import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
-import User from './user';
+import UserPage from './user'; 
 
 <div className="container">
   <div className="row">
