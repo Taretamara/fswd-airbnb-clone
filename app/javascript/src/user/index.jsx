3cee71d4354ec3612148f781c1c77499fc978e0e
SaraGolbek <145944090+SaraGolbek@users.noreply.github.com>
2024-02-08 10:23:42 -0800

    Update index.jsx

--- app/javascript/src/user/index.jsx
+++ app/javascript/src/user/index.jsx
@@ -3,9 +3,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import User from './user';
 
-document.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(
-    <User />,
-    document.body.appendChild(document.createElement('div')),
-  )
-})
+<div className="container">
+  <div className="row">
+    <div className="col-3">
+      <h3>User Dashboard</h3>
+      <p>Properties</p>
+      <p>Bookings</p>
+    </div>
+    <div className="col-9">
+      document.addEventListener('DOMContentLoaded', () => {
+        ReactDOM.render(
+          <User />,
+          document.body.appendChild(document.createElement('div')),
+        )
+      })
+     </div>
+  </div>
+</div>
