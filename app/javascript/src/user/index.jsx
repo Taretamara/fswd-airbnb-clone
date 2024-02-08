a61da080b237ad12ff0d2f2694f12d93c3bab5b5
SaraGolbek <145944090+SaraGolbek@users.noreply.github.com>
2024-02-08 11:05:53 -0800

    Update index.jsx

--- app/javascript/src/user/index.jsx
+++ app/javascript/src/user/index.jsx
@@ -3,28 +3,14 @@ import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import UserPage from './user'; 
 
-<div className="container">
-  <div className="row">
-    <div className="col-3">
-      <h3>User Dashboard</h3>
-      <div>
-      <button onClick={toggleDisplayProperties}>
-        Show Properties
-      </button>
-      <button onClick={toggleDisplayReservations}>
-        Show Reservations
-      </button>
-      {displayProperties && <PropertyList properties={properties} />}
-      {displayReservations && <ReservationList reservations={reservations} />}
-    </div>
-    </div>
-    <div className="col-9">
-      document.addEventListener('DOMContentLoaded', () => {
-        ReactDOM.render(
-          <User />,
-          document.body.appendChild(document.createElement('div')),
-        )
-      })
-     </div>
-  </div>
-</div>
+document.addEventListener('DOMContentLoaded', () => {
+  const node = document.getElementById('params');
+  const data = JSON.parse(node.getAttribute('data-params'));
+
+  ReactDOM.render(
+    <Layout></Layout>
+    <UserPage />,
+    document.body.appendChild(document.createElement('div')),
+    </Layout>
+  )
+})
