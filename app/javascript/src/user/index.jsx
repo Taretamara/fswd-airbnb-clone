5b931c32f1d6302b2fcf52eb8b9a92c09d06e938
SaraGolbek <145944090+SaraGolbek@users.noreply.github.com>
2024-02-08 10:59:58 -0800

    Update index.jsx

--- app/javascript/src/user/index.jsx
+++ app/javascript/src/user/index.jsx
@@ -1,5 +1,5 @@
 // user index.jsx
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import User from './user';
 
@@ -7,8 +7,16 @@ import User from './user';
   <div className="row">
     <div className="col-3">
       <h3>User Dashboard</h3>
-      <p>Properties</p>
-      <p>Bookings</p>
+      <div>
+      <button onClick={toggleDisplayProperties}>
+        Show Properties
+      </button>
+      <button onClick={toggleDisplayReservations}>
+        Show Reservations
+      </button>
+      {displayProperties && <PropertyList properties={properties} />}
+      {displayReservations && <ReservationList reservations={reservations} />}
+    </div>
     </div>
     <div className="col-9">
       document.addEventListener('DOMContentLoaded', () => {
