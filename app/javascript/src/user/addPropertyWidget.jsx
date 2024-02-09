41b24f1eac12d2c52c0e84a9906e8b1110cb76f7
SaraGolbek <145944090+SaraGolbek@users.noreply.github.com>
2024-02-09 13:25:09 -0800

    Update addPropertyWidget.jsx

--- app/javascript/src/user/addPropertyWidget.jsx
+++ app/javascript/src/user/addPropertyWidget.jsx
@@ -4,52 +4,49 @@ import { safeCredentials, handleErrors } from '@utils/fetchHelper';
 class AddPropertyWidget extends React.Component {
   state = {
     authenticated: false,
-    existingBookings: [],
-    startDate: null,
-    endDate: null,
-    focusedInput: null,
-    loading: false,
-    error: false,
+    
   }
 
-  componentDidMount() {
-    fetch('/api/authenticated')
-      .then(handleErrors)
-      .then(data => {
-        this.setState({
-          authenticated: data.authenticated,
-        })
-      })
+  const submitProperty = (event) => {
+    event.preventDefault();
 
-    this.getPropertyBookings();
-  }
+    const [title, getTitle] = useState('');
+    const [description, getDescription] = useState('');
+    const [city, getCity] = useState('');
+    const [country, getCountry] = useState('');
+    const [type, getType] = useState('');
+    const [price, getPrice] = useState('');
+    const [maxGuests, getMaxGuests] = useState('');
+    const [bedrooms, getBedrooms] = useState('');
+    const [beds, getBeds] = useState('');
+    const [baths, getBaths] = useState('');
 
-
-  submitBooking = (e) => {
-    if (e) { e.preventDefault(); }
-    const { startDate, endDate } = this.state;
-    console.log(startDate.format('MMM DD YYYY'), endDate.format('MMM DD YYYY'));
-    fetch(`/api/bookings`, safeCredentials({
+    fetch('/api/properties', safeCredentials ({
       method: 'POST',
-        body: JSON.stringify({
-          booking: {
-            property_id: this.props.property_id,
-            start_date: startDate.format('MMM DD YYYY'),
-            end_date: endDate.format('MMM DD YYYY'),
-          }
-        })
+      body: JSON.stringify({
+        property: {
+          title: title,
+          description: description,
+          city: city,
+          country: country,
+          property_type: type,
+          price_per_night: price,
+          max_guests: maxGuests,
+          bedrooms: bedrooms,
+          beds: beds,
+          baths: baths,
+        }
+      })
     }))
       .then(handleErrors)
-      .then(response => {
-        return this.initiateStripeCheckout(response.booking.id)
+      .then(data => {
+        console.log(data);
+        refreshFeed();
       })
-      .catch(error => {
-        console.log(error);
-      })
-  }
+  };
 
 render () {
-  const { authenticated, startDate, endDate, focusedInput } = this.state;
+  const { authenticated } = this.state;
   if (!authenticated) {
     return (
       <div className="border p-4 mb-4">
@@ -61,28 +58,29 @@ render () {
 
   return (
     <div className="border p-4 mb-4">
-      <form onSubmit={this.submitProperty}>
-       <input type="text" placeholder="property title"></input>
-        <input type="text" placeholder="property description"></input>
+      <form onSubmit={submitProperty}>
+       <input type="text" placeholder="Property title" onChange={(event) => getTitle(event.target.value)}></input>
+        <input type="text" placeholder="Property description" onChange={(event) => getDescription(event.target.value)}></input>
         <hr />
         <h5>Property Location</h5>
-        <input type="text" placeholder="city"></input>
-        <input type="text" placeholder="country"></input>
+        <input type="text" placeholder="City" onChange={(event) => getCity(event.target.value)}></input>
+        <input type="text" placeholder="Country" onChange={(event) => getCountry(event.target.value)}></input>
         <hr />
         <label for="propertyType">Select Your Property Type</label>
-        <select id="propertyType">
-          <option value="studio"></option>
-          <option value="room in hotel"></option>
-          <option value="private room in apartment"></option>
-          <option value="entire apartment"></option>
-          <option value="endtire condiminium"></option>
-          <option value="entire house"></option>
+        <select id="propertyType" onChange={(event) => getType(event.target.value)}>
+          <option value="" disabled selected>Select your property type</option>
+          <option value="studio">Studio</option>
+          <option value="room in hotel">Room in hotel</option>
+          <option value="private room in apartment">Private room in apartment</option>
+          <option value="entire apartment">Entire apartment</option>
+          <option value="entire condiminium">Entire condiminimum</option>
+          <option value="entire house">Entire house</option>
         </select>
-        <label for="price">Price per night:</label><input type="number" id="price"></input>
-        <label for="guests">Max number of guests:</label><input type="number" id="guests"></input>
-        <label for="bedrooms">Number of bedrooms:</label><input type="number" id="bedrooms"></input>
-        <label for="beds">Number of beds:</label><input type="number" id="beds"></input>
-        <label for="baths">Number of bathrooms:</label><input type="number" id="baths"></input>
+        <label for="price" onChange={(event) => getPrice(event.target.value)}>Price per night:</label><input type="number" id="price"></input>
+        <label for="guests" onChange={(event) => getMaxGuests(event.target.value)}>Max number of guests:</label><input type="number" id="guests"></input>
+        <label for="bedrooms" onChange={(event) => getBedrooms(event.target.value)}>Number of bedrooms:</label><input type="number" id="bedrooms"></input>
+        <label for="beds" onChange={(event) => getBeds(event.target.value)}>Number of beds:</label><input type="number" id="beds"></input>
+        <label for="baths" onChange={(event) => getBaths(event.target.value)}>Number of bathrooms:</label><input type="number" id="baths"></input>
         // add picture upload here
         <button type="submit" className="btn btn-large btn-danger btn-block">Add</button>
       </form>
