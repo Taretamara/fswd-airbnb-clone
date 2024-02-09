9a114203514b2634916bde3f2e89f6362bc885d5
SaraGolbek <145944090+SaraGolbek@users.noreply.github.com>
2024-02-09 13:31:04 -0800

    Update PropertyList.jsx

--- app/javascript/src/user/PropertyList.jsx
+++ app/javascript/src/user/PropertyList.jsx
@@ -9,7 +9,9 @@ const PropertyList = () => {
       <h3>Your Properties</h3>
     </div>
     <div className="row ms-5">
-      <AddPropertyWidget />
+      <p> Do you want to add a </p>
+      <a href={`/addPropertyWidget?redirect_url=${window.location.pathname}`}>property</a>
+      <p>?</p>
     </div>
   );
 };
