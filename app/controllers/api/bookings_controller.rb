4d5ed1397d5d4c794c3a4e896394738472211a9a
SaraGolbek <145944090+SaraGolbek@users.noreply.github.com>
2024-02-12 11:20:12 -0800

    Update bookings_controller.rb

--- app/controllers/api/bookings_controller.rb
+++ app/controllers/api/bookings_controller.rb
@@ -3,13 +3,15 @@ module Api
     def create
       token = cookies.signed[:airbnb_session_token]
       session = Session.find_by(token: token)
-      return render json: { error: 'user not logged in' }, status: :unauthorized if !session
+      return render json: { error: 'user not logged in' }, status: :unauthorized unless session
 
-      property = Property.find_by(id: params[:booking][:property_id])
-      return render json: { error: 'cannot find property' }, status: :not_found if !property
+      user = session.user if session
+      property = user.properties.find_by(id: params[:booking][:property_id])
+      return render json: { error: 'cannot find property' }, status: :not_found unless property
 
       begin
-        @booking = Booking.create({ user_id: session.user.id, property_id: property.id, start_date: params[:booking][:start_date], end_date: params[:booking][:end_date]})
+        @booking = user.bookings.create(property_id: property.id, start_date: params[:booking][:start_date], end_date: params[:booking][:end_date])
+        
         render 'api/bookings/create', status: :created
       rescue ArgumentError => e
         render json: { error: e.message }, status: :bad_request
@@ -18,9 +20,9 @@ module Api
 
     def get_property_bookings
       property = Property.find_by(id: params[:id])
-      return render json: { error: 'cannot find property' }, status: :not_found if !property
+      return render json: { error: 'cannot find property' }, status: :not_found unless property
 
-      @bookings = property.bookings.where("end_date > ? ", Date.today)
+      @bookings = property.bookings.where('end_date > ? ', Date.today)
       render 'api/bookings/index'
     end
 
