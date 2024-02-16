c1410a79a836e1c58d3e652f4d350c442ed07db2
Sara <golbeksara@gmail.com>
2024-02-15 20:15:45 -0800

    editing properties

--- app/javascript/src/user/propertyWidget.jsx
+++ app/javascript/src/user/propertyWidget.jsx
@@ -16,7 +16,7 @@ class PropertyWidget extends React.Component {
       bedrooms: '',
       beds: '',
       baths: '',
-      propertyId: this.props.propertyId || null,
+      id: this.props.id || null,
     };
 
     this.handleChange = this.handleChange.bind(this);
@@ -30,7 +30,7 @@ class PropertyWidget extends React.Component {
   }
 
   updateProperty() {
-    fetch(`/api/properties/${this.state.propertyId}/update`, safeCredentials ({
+    fetch(`/api/properties/update`, safeCredentials ({
       method: 'POST',
       body: JSON.stringify({
         property: {
@@ -84,22 +84,22 @@ class PropertyWidget extends React.Component {
   }
 
   render() {
-    const { title, description, city, country, type, price, maxGuests, bedrooms, beds, baths } = this.state;
+    const { title, description, city, country, type, price, maxGuests, bedrooms, beds, baths, id } = this.state;
 
     return (
       <React.Fragment>
         <div className="border rounded shadow-sm p-4 mb-4">
           <h4 className=" mb-3">New Property</h4>
-          <form onSubmit={(e) => { e.preventDefault(); this.state.propertyId ? this.updateProperty() : this.submitProperty(); }}>
-            <input type="text" name="title" className="form-control mb-3" placeholder="Property title" value={title} onChange={this.handleChange} required />
-            <input type="text" name="description" className="form-control mb-3" placeholder="Property description" value={description} onChange={this.handleChange} required />
+          <form onSubmit={(e) => { e.preventDefault(); this.state.id ? this.updateProperty() : this.submitProperty(); }}>
+            <input type="text" name="title" className="form-control mb-3" placeholder="Property title" value={title} onChange={this.handleChange} required={!this.state.id} />
+            <input type="text" name="description" className="form-control mb-3" placeholder="Property description" value={description} onChange={this.handleChange} required={!this.state.id} />
             <hr />
             <h6>Property Location</h6>
-            <input type="text" name="city" className="form-control mb-3" placeholder="City" value={city} onChange={this.handleChange} />
-            <input type="text" name="country" className="form-control mb-3" placeholder="Country" value={country} onChange={this.handleChange} />
+            <input type="text" name="city" className="form-control mb-3" placeholder="City" value={city} onChange={this.handleChange} required={!this.state.id}/>
+            <input type="text" name="country" className="form-control mb-3" placeholder="Country" value={country} onChange={this.handleChange} required={!this.state.id}/>
             <hr />
             <h6>Property Details</h6>
-            <select id="propertyType" name="type" className="form-control mb-3" value={type} onChange={this.handleChange} required>
+            <select id="propertyType" name="type" className="form-control mb-3" value={type} onChange={this.handleChange} required={!this.state.id}>
               <option value="" disabled>Select your property type</option>
               <option value="studio">Studio</option>
               <option value="room in hotel">Room in hotel</option>
@@ -110,12 +110,12 @@ class PropertyWidget extends React.Component {
             </select>
             <div className="input-group  mb-3">
               <span className="input-group-text">$</span>
-              <input type="number" id="price" name="price" className="form-control" value={price} onChange={this.handleChange} placeholder="Nightly Rate" required />
+              <input type="number" id="price" name="price" className="form-control" value={price} onChange={this.handleChange} placeholder="Nightly Rate" required={!this.state.id} />
             </div>
-            <input type="number" name="maxGuests" id="guests" className="form-control mb-3" value={maxGuests} onChange={this.handleChange} placeholder="Max Guests" required />
-            <input type="number" name="bedrooms" id="bedrooms" className="form-control mb-3" value={bedrooms} onChange={this.handleChange} placeholder="Number of Bedrooms" required />
-            <input type="number" name="beds" id="beds" className="form-control mb-3" value={beds} onChange={this.handleChange} placeholder="Number of Beds" required />
-            <input type="number" name="baths" id="baths" className="form-control mb-3" value={baths} onChange={this.handleChange} placeholder="Number of Baths" required />
+            <input type="number" name="maxGuests" id="guests" className="form-control mb-3" value={maxGuests} onChange={this.handleChange} placeholder="Max Guests" required={!this.state.id} />
+            <input type="number" name="bedrooms" id="bedrooms" className="form-control mb-3" value={bedrooms} onChange={this.handleChange} placeholder="Number of Bedrooms" required={!this.state.id} />
+            <input type="number" name="beds" id="beds" className="form-control mb-3" value={beds} onChange={this.handleChange} placeholder="Number of Beds" required={!this.state.id} />
+            <input type="number" name="baths" id="baths" className="form-control mb-3" value={baths} onChange={this.handleChange} placeholder="Number of Baths" required={!this.state.id} />
             <button type="submit" className="btn btn-large btn-danger btn-block mb-3">Add</button>
           </form>
           <button type="cancel" className="btn btn-large btn-danger btn-block" onClick={this.props.toggle}>Cancel</button>
